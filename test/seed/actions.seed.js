1c6e3974b2bcd1a92906a367ad81769167891535
jkopen <jknoop@wice.de>
2021-03-03 16:56:20 +0100

    Starting unit tests

--- test/seed/actions.seed.js
+++ test/seed/actions.seed.js
@@ -1,278 +1,44 @@
 const nock = require('nock');
 
-const createPersonSuccessful = nock('https://api.snazzycontacts.com/api/operation')
+const entryId = 1;
+const url = 'https://rest.cleverreach.com/v3';
+const groups = [{
+  id: '1',
+  name: 'Testgruppe',
+}];
+
+const createReceiverSuccessful = nock(`${url}/groups.json/${groups[0].id}/receivers`)
   .post('')
-  .query({
-    type: 'person',
-  })
-  .reply(200, {
-    eventId: 'yj5o81jvazco6v',
-    eventName: 'PersonCreated',
-    meta: {
-      role: 'USER',
-      user: '5c7f8c5da3c613001042f618',
-      tenant: '5c7f8c5ea3c613001042f619',
-      username: 'admin@wice.de',
-    },
-    timeStamp: 1557063409787,
-    causalId: 0,
-    payload: {
-      firstName: 'John',
-      lastName: 'Doe',
-      uid: 'yj5o81jvazco6s',
-      gender: '',
-      jobTitle: '',
-      nickname: '',
-      displayName: '',
-      middleName: '',
-      salutation: '',
-      title: '',
-      birthday: '',
-    },
-  });
+  .reply(200, {});
 
-const createPersonFailed = nock('https://api.snazzycontacts.com/api/operation')
+const createReceiverFailed = nock(`${url}/groups.json/0/receivers`)
   .post('')
-  .query({
-    type: 'person',
-  })
-  .reply(400, 'Data does not match schema!');
+  .reply(500, 'Group not found');
 
-const deletePersonSuccessful = nock('https://api.snazzycontacts.com/api/person/8sjwp1jvdhswq2')
-  .delete('')
-  .reply(200, {
-    id: '3gbbk1jmq3nv5q',
-    eventName: 'PersonDeleted',
-    timeStamp: '1537791877918',
-    meta: {
-      role: 'TENANT_ADMIN',
-      user: '5d945793728470011e122fa',
-      tenant: '31d21937284700730d73j8',
-      username: 'example@wice.de',
-    },
-    causalId: '0',
-    payload: {
-      uid: '8sjwp1jvdhswq2',
-    },
-  });
 
-const deletePersonFailed = nock('https://api.snazzycontacts.com/api/person/')
-  .delete('')
-  .reply(400, 'Uid is not defined!');
-
-const deletePersonNotFound = nock('https://api.snazzycontacts.com/api/person/111111')
-  .delete('')
-  .reply(204);
-
-const createOrganizationSuccessful = nock('https://api.snazzycontacts.com/api/operation')
+const updateReceiverSuccessful = nock(`${url}/groups.json/${groups[0].id}/receivers/1`)
   .post('')
-  .query({
-    type: 'organization',
-  })
-  .reply(200, {
-    eventId: '3gbds1jvduhqiw',
-    eventName: 'OrganizationCreated',
-    meta: {
-      role: 'USER',
-      user: '5c7f8c5da3c613001042f618',
-      tenant: '5c7f8c5ea3c613001042f619',
-      username: 'admin@wice.de',
-    },
-    timeStamp: 1557236646537,
-    causalId: 0,
-    payload: {
-      name: 'Wice GmbH',
-      logo: 'Logo',
-      addresses: [
-        {
-          street: 'Wendenstr',
-          streetNumber: '120',
-          unit: 'Hammerbrook',
-          zipcode: '20537',
-          city: 'Hamburg',
-          district: 'HH-Hamburg',
-          region: 'Hamburg',
-          country: 'Germany',
-          countryCode: '0049',
-          primaryContact: 'true',
-          description: 'Private Address',
-          uid: '3gbds1jvduhqiu',
-        },
-      ],
-      uid: '3gbds1jvduhqit',
-    },
-  });
+  .reply(200, {});
 
-const createOrganizationFailed = nock('https://api.snazzycontacts.com/api/operation')
+const updateReceiverFailed = nock(`${url}/groups.json/0/receivers/0`)
   .post('')
-  .query({
-    type: 'organization',
-  })
-  .reply(400, 'Data does not match schema!');
+  .reply(500, 'Entry not found');
 
-const deleteOrganizationSuccessful = nock('https://api.snazzycontacts.com/api/organization/2jkwerjvdhswq2')
+
+const deleteReceiverSuccessful = nock(`${url}/groups.json/${groups[0].id}/receivers/${entryId}`)
   .delete('')
-  .reply(200, {
-    id: '5dlkk1jmq3nv5q',
-    eventName: 'OrganizationDeleted',
-    timeStamp: '1537791877918',
-    meta: {
-      role: 'TENANT_ADMIN',
-      user: '5d94573j7g37p0011e122fa',
-      tenant: '31d2193hj384kl830d73j8',
-      username: 'example@wice.de',
-    },
-    causalId: '0',
-    payload: {
-      uid: '2jkwerjvdhswq2',
-    },
-  });
+  .reply(200, {});
 
-const deleteOrganizationFailed = nock('https://api.snazzycontacts.com/api/organization/')
+const deleteReceiverFailed = nock(`${url}/groups.json/${groups[0].id}/receivers/7`)
   .delete('')
-  .reply(400, 'Uid is not defined!');
+  .reply(500, 'Can not delete. Entry not found');
 
-const deleteOrganizationNotFound = nock('https://api.snazzycontacts.com/api/organization/222222')
-  .delete('')
-  .reply(204);
-
-const getPerson = nock('https://api.snazzycontacts.com/api/person/25mop1jxq2ss3x')
-  .get('')
-  .reply(200, {
-    _id: '5d1f429dbbe76eeb57af028e',
-    isUser: false,
-    firstName: 'Yahoouser',
-    lastName: 'Accountname',
-    photo: 'https://cdn3.iconfinder.com/data/icons/ultimate-social/150/43_yahoo-512.png',
-    uid: '25mop1jxq2ss3x',
-    gender: '',
-    jobTitle: '',
-    nickname: '',
-    displayName: '',
-    middleName: '',
-    salutation: '',
-    title: '',
-    birthday: '',
-    lastUpdate: '1562409837891',
-    updateEvent: '7q9m1jxreh6ir',
-    meta: {
-      role: 'USER',
-      user: '5d1f42743805f3001257392e',
-      tenant: '5d1f420d3805f3001257392d',
-      username: 'admin@wice.de',
-    },
-    addresses: [],
-    contactData: [],
-    categories: [
-      {
-        uid: '25mop1jxq2pp3e',
-        label: 'Customer',
-      },
-    ],
-    relations: [],
-    __v: 0,
-    lastUpdateBy: null,
-    lastUpdateById: null,
-  });
-
-const updatePerson = nock('https://api.snazzycontacts.com/api/operation/person')
-  .put('')
-  .query({
-    type: 'person',
-  })
-  .reply(200, {
-    eventId: 'o0d48u31jzxuwspc',
-    eventName: 'PersonLastNameUpdated',
-    meta: {
-      role: 'USER',
-      user: '5d1f420cf7a2170011747690',
-      tenant: '5d1f420d3805f3001257392d',
-      username: 'admin@wice.de',
-    },
-    timeStamp: 1567153802064,
-    causalId: 0,
-    payload: {
-      uid: '25mop1jzwjc4by',
-      lastName: 'Stevenson',
-    },
-  });
-
-const personResolve = nock('https://api.snazzycontacts.com/api/person/902jf1jxq2ss3x')
-  .get('')
-  .reply(200, {
-    _id: '83jd729dbbe76eeb57af028e',
-    isUser: false,
-    firstName: 'Jane',
-    lastName: 'Brown',
-    photo: 'https://cdn3.iconfinder.com/data/icons/ultimate-social/150/43_yahoo-512.png',
-    uid: '902jf1jxq2ss3x',
-    gender: 'female',
-    jobTitle: '',
-    nickname: '',
-    displayName: '',
-    middleName: '',
-    salutation: '',
-    title: '',
-    birthday: '',
-    lastUpdate: '1562409837891',
-    updateEvent: '7q9m1jxreh6ir',
-    meta: {
-      role: 'USER',
-      user: '5d1f42743805f3001257392e',
-      tenant: '5d1f420d3805f3001257392d',
-      username: 'jane.brown@yahoo.com',
-    },
-    addresses: [{
-      street: 'Some Str.',
-      streetNumber: '456',
-      city: 'Cologne',
-    }],
-    contactData: [
-      {
-        type: 'phone',
-        value: '123456',
-        description: 'private',
-      },
-      {
-        type: 'fax',
-        value: '68790',
-        description: 'private',
-      },
-    ],
-    categories: [
-      {
-        uid: '25mop1jxq2pp3e',
-        label: 'Customer',
-      },
-    ],
-    relations: [],
-    __v: 0,
-    lastUpdateBy: null,
-    lastUpdateById: null,
-  });
-
-const getPersonFailed = nock('https://api.snazzycontacts.com/api/person/123asd')
-  .get('')
-  .reply(204);
-
-const getPersonNoToken = nock('https://api.snazzycontacts.com/api/person/98hkp1jxq2ss3x')
-  .get('')
-  .reply(401);
 
 module.exports = {
-  createPersonSuccessful,
-  createPersonFailed,
-  deletePersonSuccessful,
-  deletePersonFailed,
-  deletePersonNotFound,
-  createOrganizationSuccessful,
-  createOrganizationFailed,
-  deleteOrganizationSuccessful,
-  deleteOrganizationFailed,
-  deleteOrganizationNotFound,
-  updatePerson,
-  getPerson,
-  getPersonFailed,
-  getPersonNoToken,
-  personResolve,
+  createReceiverSuccessful,
+  createReceiverFailed,
+  updateReceiverSuccessful,
+  updateReceiverFailed,
+  deleteReceiverSuccessful,
+  deleteReceiverFailed,
 };
